9993ad8a5741903ada37c1e3e8afbcf22d554ca7
Nishant Soni <nishant@npsoni.com>
2020-11-11 13:33:20 +0530

    Update myscript.ps1

--- myscript.ps1
+++ myscript.ps1
@@ -1,3 +1,1 @@
 Install-WIndowsFeature -name web-server -IncludeManagementTools
-Expand-Archive -LiteralPath C:\Archives\Invoices.Zip -DestinationPath C:\ InvoicesUnzipped
-Copy-Item 'D:\temp\Test Folder' -Destination 'D:\temp\Test Folder1'
