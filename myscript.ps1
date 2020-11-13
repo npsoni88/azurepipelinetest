9f2350c1e71b5e1362852ec87a586c3219bdefe3
Nishant Soni <nishant@npsoni.com>
2020-11-13 23:46:47 +0530

    Update myscript.ps1

--- myscript.ps1
+++ myscript.ps1
@@ -1,2 +1,2 @@
 Add-WindowsFeature Web-Server
-Set-Content -Path "C:\inetpub\wwwroot\Default.htm" -Value "Hello World from host $($env:computername) ! - updated by Nishant"
+Set-Content -Path "C:\inetpub\wwwroot\Default.htm" -Value "Hello World from host $($env:computername) ! - updated by Nishant v4"
