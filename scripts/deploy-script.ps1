99dcceb61abe7dc6715e8992ab55d38e5aa25558
Nishant Soni <nishant@npsoni.com>
2020-11-13 18:26:49 +0530

    Update deploy-script.ps1

--- scripts/deploy-script.ps1
+++ scripts/deploy-script.ps1
@@ -1,3 +1,3 @@
 Install-WIndowsFeature -name web-server -IncludeManagementTools
 New-Item C:\inetpub\wwwroot\index.html
-Set-Content C:\inetpub\wwwroot\index.html 'Welcome to Powershell'
+Set-Content C:\inetpub\wwwroot\index.html 'Welcome to Powershell updated'
